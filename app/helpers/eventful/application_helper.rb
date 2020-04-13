5b609a441620acadd8a18f8ae4657badd2e38f27
Andrew Ek <andrewek@gmail.com>
2020-04-13 00:54:50 -0500

    More cleanup

--- app/helpers/eventful/application_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Eventful
-  # Base Helper
-  module ApplicationHelper
-  end
-end
--- app/jobs/eventful/application_job.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Eventful
-  class ApplicationJob < ActiveJob::Base
-  end
-end
